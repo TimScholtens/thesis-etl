db7efa0515b79b2a698e94c0b849662c9a59c006
Tim <timscholtens@hotmail.com>
2020-06-01 22:17:42 +0200

    intermediate update

--- etl/transform/transformers/vlinderstichting.py
+++ etl/transform/transformers/vlinderstichting.py
@@ -1,25 +1,43 @@
 from etl.transform.transformers.base import Base
 from pathlib import Path
 from datetime import datetime
-import csv
+import pandas as pd
+
 
 class Vlinderstichting(Base):
 
     def transform(self, extract_directory, transform_directory):
+        column_mapping = {
+            "dag": "day",
+            "maand": "month",
+            "jaar": "year",
+            "stadium": "stage",
+            "x": "longitude",
+            "y": "latitude"
+        }
+
+        dtypes = {
+            "day": "uint8",
+            "month": "uint8",
+            "year": "uint16",
+            "stage": "categorical",
+            "longitude": "float32",
+            "latitude": "float32"
+        }
+
         file_path = extract_directory / 'epr_20200521.csv'
+        df = pd.read_csv(
+            file_path,
+            usecols=list(column_mapping),
+            dtype=dtypes,
+            header=0
+        )
 
-        with open(file_path) as input_file:
-            csv_reader = csv.DictReader(input_file, delimiter=';')
+        # Set date column
+        df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
 
-            # Note: coordinates are in eps:28992
+        print(df)
 
-            oak_processionary_moths = [dict(
-                date=datetime.date(row['"jaar"'], row['"maand"'], row['"dag"']),
-                stage=row['stadium'],
-                geometry=row['']
-            ) for row in csv_reader]
-
-
-
+        # Convert "rijksdriehoekcoordinaten" EPSG 28992 to EPSG 4326 (WSG 84)
 
 
