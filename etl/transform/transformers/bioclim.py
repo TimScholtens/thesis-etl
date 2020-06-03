b4d46c75e0d13a2d5ff5dd5f26f7a1c184c92161
Tim <timscholtens@hotmail.com>
2020-06-03 11:57:26 +0200

    refactored transformer

--- etl/transform/transformers/bioclim.py
+++ etl/transform/transformers/bioclim.py
@@ -4,6 +4,8 @@ from sklearn.neighbors import BallTree, KNeighborsRegressor
 import json
 import geopandas as gpd
 import pandas as pd
+from abc import ABC, abstractmethod
+import numpy as np
 
 
 def save_dataframe_to_csv(path, dataframe):
@@ -12,6 +14,72 @@ def save_dataframe_to_csv(path, dataframe):
         Path.mkdir(path.parent)
 
     dataframe.to_csv(path, index=False)
+
+
+def load_weather_station_data(extract_directory):
+    # Rename to more readable names,
+    # note: only select columns which are related to BIOCLIM, being temperature and perception
+    column_mapping = {
+        'STN': 'station_id',
+        'YYYYMMDD': 'date',
+        'TG': 'temperature_avg',
+        'TN': 'temperature_min',
+        'TX': 'temperature_max',
+        'SQ': 'sunshine_duration',
+        'Q': 'sunshine_radiation',
+        'DR': 'rain_duration',
+        'RH': 'rain_sum',
+        'UG': 'humidity_avg',
+        'UX': 'humidity_max',
+        'UN': 'humidity_min'
+    }
+
+    dtypes = {
+        "STN": "uint16",
+        "YYYYMMDD": "str",
+        "TG": "float32",
+        "TN": "float32",
+        "TX": "float32",
+        "SQ": "float32",
+        "Q": "float32",
+        "DR": "float32",
+        "RH": "float32",
+        "UG": "float32",
+        "UX": "float32",
+        "UN": "float32"
+    }
+
+    # Load
+    df_weather_station_data = pd.read_csv(
+        extract_directory / 'station_data.csv',
+        dtype=dtypes,
+        usecols=list(column_mapping),
+        header=40
+    )
+
+    # Rename to more meaningful names
+    df_weather_station_data = df_weather_station_data.rename(columns=column_mapping)
+
+    # Set datetimeindex
+    df_weather_station_data['date'] = pd.to_datetime(df_weather_station_data['date'])
+
+    # Transform temperature, sunshine and rain to decimal values (check description of this function)
+    df_weather_station_data[
+        ['temperature_avg',
+         'temperature_min',
+         'temperature_max',
+         'sunshine_duration',
+         'sunshine_radiation',
+         'rain_duration']
+    ] = df_weather_station_data[
+            ['temperature_avg',
+             'temperature_min',
+             'temperature_max',
+             'sunshine_duration',
+             'sunshine_radiation',
+             'rain_duration']] / 10
+
+    return df_weather_station_data
 
 
 def load_weather_station_locations(extract_directory):
@@ -36,169 +104,171 @@ def load_weather_station_locations(extract_directory):
     df_weather_station_location = pd.read_csv(
         extract_directory / 'station_locations.csv',
         dtype=dtypes,
-        usecols=list(dtypes),
-        names=list(dtypes),
+        usecols=list(column_mapping),
         header=0
     )
 
+    # Rename to more meaningful names
+    df_weather_station_location = df_weather_station_location.rename(columns=column_mapping)
+
     return df_weather_station_location
-
-
-def load_weather_station_data(extract_directory):
-
-    # Load
-    df_weather_station_data = gpd.read_file(extract_directory / 'station_data.csv')
-
-    # Rename to more readable names,
-    # note: only select columns which are related to BIOCLIM being temperature and perception
-    column_mapping = {
-        'STN': 'station_id',
-        'YYYYMMDD': 'date',
-        'TG': 'temperature_avg',
-        'TN': 'temperature_min',
-        'TX': 'temperature_max',
-        'SQ': 'sunshine_duration',
-        'Q': 'sunshine_radiation',
-        'DR': 'rain_duration',
-        'RH': 'rain_sum',
-        'UG': 'humidity_avg',
-        'UX': 'humidity_max',
-        'UN': 'humidity_min'}
-
-    # Filter columns
-    columns_of_interest = [column for column in column_mapping.keys()]
-    df_weather_station_data = df_weather_station_data[columns_of_interest]
-
-    # Rename
-    df_weather_station_data = df_weather_station_data.rename(columns=column_mapping)
-
-    # Set data types
-    df_weather_station_data['station_id'] = pd.to_numeric(df_weather_station_data['station_id'], errors='coerce')
-    df_weather_station_data['date'] = pd.to_datetime(df_weather_station_data['date'])
-    df_weather_station_data['temperature_avg'] = pd.to_numeric(df_weather_station_data['temperature_avg'],
-                                                               errors='coerce')
-    df_weather_station_data['temperature_min'] = pd.to_numeric(df_weather_station_data['temperature_min'],
-                                                               errors='coerce')
-    df_weather_station_data['temperature_max'] = pd.to_numeric(df_weather_station_data['temperature_max'],
-                                                               errors='coerce')
-    df_weather_station_data['sunshine_duration'] = pd.to_numeric(df_weather_station_data['sunshine_duration'],
-                                                                 errors='coerce')
-    df_weather_station_data['sunshine_radiation'] = pd.to_numeric(df_weather_station_data['sunshine_radiation'],
-                                                                  errors='coerce')
-    df_weather_station_data['rain_duration'] = pd.to_numeric(df_weather_station_data['rain_duration'], errors='coerce')
-    df_weather_station_data['rain_sum'] = pd.to_numeric(df_weather_station_data['rain_sum'], errors='coerce')
-    df_weather_station_data['humidity_avg'] = pd.to_numeric(df_weather_station_data['humidity_avg'], errors='coerce')
-    df_weather_station_data['humidity_max'] = pd.to_numeric(df_weather_station_data['humidity_max'], errors='coerce')
-
-    # Transform temperature, sunshine and rain to decimal values (check description of this function)
-    df_weather_station_data['temperature_avg'] = df_weather_station_data['temperature_avg'] / 10
-    df_weather_station_data['temperature_min'] = df_weather_station_data['temperature_min'] / 10
-    df_weather_station_data['temperature_max'] = df_weather_station_data['temperature_max'] / 10
-    df_weather_station_data['sunshine_duration'] = df_weather_station_data['sunshine_duration'] / 10
-    df_weather_station_data['sunshine_radiation'] = df_weather_station_data['sunshine_radiation'] / 10
-    df_weather_station_data['rain_duration'] = df_weather_station_data['rain_duration'] / 10
-
-    return df_weather_station_data
 
 
 def load_township_data(extract_directory):
     return gpd.read_file(extract_directory / 'townships.json')
 
 
-class BioClim_1(Base):
+def calculate_townships_centroids(df_township):
+    # Get centroids
+    df_township_centroid = gpd.GeoDataFrame(df_township.centroid, columns=['centroid'], geometry='centroid')
 
-    def transform(self, extract_directory, transform_directory):
-        from config import FINAL_TRANSFORMATION_ID
+    # Concat
+    df_township_centroid = pd.concat([df_township_centroid, df_township[['code', 'name']]], axis=1)
 
-        def calculate_townships_centroids(df_township):
-            # Get centroids
-            df_township_centroid = gpd.GeoDataFrame(df_township.centroid, columns=['centroid'], geometry='centroid')
+    return df_township_centroid
 
-            # Concat
-            df_township_centroid = pd.concat([df_township_centroid, df_township[['code', 'name']]], axis=1)
 
-            return df_township_centroid
+def interpolate(X, X_interpolate_locations, y):
+    """
+    X: training points
+    X_interpolate: points which have to be interpolated
+    y: y training values
 
-        def yearly_avg_temperature_weather_station_data(weather_station_data):
-            # Todo: dirty code, refactor
-            agg_data = weather_station_data.groupby(['station_id', pd.Grouper(key='date', freq='Y')]).mean()
-            agg_data = agg_data.reset_index()
-            agg_data['year'] = agg_data['date'].dt.year
+    returns: numpy array holding all predicted values
+    """
+    knn_regressor = KNeighborsRegressor(metric='haversine', algorithm='ball_tree', weights='distance', leaf_size=2)
+    knn_regressor.fit(X, y)
 
-            return agg_data  # Todo: No clue how this works
+    return knn_regressor.predict(X_interpolate_locations)[:, 0]
 
-        def interpolate(df_townships_centroids, ws_yearly_average_temperature, ws_location):
-            """
-            Fit k-nearest-neighbors ball tree having parameters:
-             - For interpolation use Inverse Distance Weighting to determine weights of each weather station
-             - For interpolation use Haversine to calculate distance between given point and weather station
-            """
 
-            # Loop through each year and fit KNN tree,
-            # then calculate for each township centroid the interpolated temperature for that year.
-            # return -> township | year | temperature
+class BioClim(Base, ABC):
 
-            # Create new dataframe for holding interpolated temperatures
-            df_townships_interpolated = pd.DataFrame(columns=['township', 'interpolated_temperature', 'year'])
+    def __init__(self, time_window_frequency):
+        self._time_window_frequency = time_window_frequency
 
-            for year in set(ws_yearly_average_temperature['year'].values):
-                # Get ws coordinates and temperature for given year
-                year_filter = ws_yearly_average_temperature['year'] == year
-                ws_temperature = ws_yearly_average_temperature.where(year_filter).dropna()
-                ws_temperature_location = ws_temperature.merge(ws_location, left_on='station_id', right_on='station_id')
+        self._X_interpolate_labels = None
+        self._X_interpolate_locations = None
 
-                # Fit tree
-                ws_coords = ws_temperature_location[['longitude', 'latitude']].values  # X
-                ws_temperatures = ws_temperature_location['temperature_avg'].values  # y
-                knn_regressor = KNeighborsRegressor(metric='haversine', algorithm='ball_tree', weights='distance',
-                                                    leaf_size=2)
-                knn_regressor.fit(ws_coords, ws_temperatures)
-
-                # Calculate interpolated temperature for each township centroid
-                def interpolate_temperature(point):
-                    latitude = point.x
-                    longitude = point.y
-
-                    return knn_regressor.predict([[latitude, longitude]])[0]
-
-                interpolated_townships = [dict(year=year,
-                                               township=row['name'],
-                                               interpolated_temperature=interpolate_temperature(row['centroid']))
-                                          for index, row in df_townships_centroids.iterrows()]
-
-                df_townships_interpolated = df_townships_interpolated.append(interpolated_townships)
-
-            return df_townships_interpolated
-
-        # Load data
+    def load_extract_files(self, extract_directory, transform_directory):
+        # Merge weather station data and their locations
         weather_station_data = load_weather_station_data(extract_directory)
         weather_station_locations = load_weather_station_locations(extract_directory)
-        townships = load_township_data(extract_directory)
 
-        # Calculate centroids for each township, save output
-        df_townships_centroids = calculate_townships_centroids(townships)
-        save_dataframe_to_csv(path=transform_directory / 'township_centroids.csv', dataframe=df_townships_centroids)
+        self.weather_station_data_and_locations = weather_station_data.merge(weather_station_locations,
+                                                                             left_on='station_id',
+                                                                             right_on='station_id')
 
-        # Filter columns for each weather station
-        df_ws_avg_temperature = weather_station_data[['temperature_avg', 'station_id', 'date']]
+        # Set townships
+        self.townships = load_township_data(extract_directory)
 
-        # Filter out temperature records having no value
-        df_ws_avg_temperature = df_ws_avg_temperature.dropna()
+    @property
+    def time_window_frequency(self):
+        return self._time_window_frequency
 
-        # Calculate yearly average temperature for each weather station, save output
-        df_ws_yearly_avg_temperature = yearly_avg_temperature_weather_station_data(df_ws_avg_temperature)
-        save_dataframe_to_csv(path=transform_directory / 'weather_station_avg_temperature.csv',
-                              dataframe=df_ws_yearly_avg_temperature)
+    @property
+    def X_interpolate_locations(self):
+        """
+            Points (lon,lat) which have to be interpolated.
+            returns numpy array containing all long, lat coordinates which have to be interpolated.
+        """
+
+        if self._X_interpolate_locations is None:
+            # Calculate centroid
+            df_townships_centroids = calculate_townships_centroids(self.townships)
+
+            df_townships_centroids['longitude'] = df_townships_centroids['centroid'].apply(lambda point: point.x)
+            df_townships_centroids['latitude'] = df_townships_centroids['centroid'].apply(lambda point: point.y)
+
+            self._X_interpolate_locations = df_townships_centroids[['longitude', 'latitude']].values
+
+        return self._X_interpolate_locations
+
+    @property
+    def X_interpolate_labels(self):
+        """
+            Labels of points which have to be interpolated.
+            returns numpy array containing all point labels.
+        """
+
+        if self._X_interpolate_labels is None:
+            self._X_interpolate_labels = self.townships[['name']].values
+
+        return self._X_interpolate_labels[:, 0]
+
+    def X(self, dataframe):
+        """
+            Returns known points.
+            return numpy array ([long,lat]) of known points
+        """
+        return dataframe[['longitude', 'latitude']].values
+
+    @abstractmethod
+    def y(self, dataframe):
+        """
+            Different per BIOCLIM variable; training data response variable
+        """
+        pass
+
+    def training_data(self, time_window_frequency):
+
+        grouped_training_data = self.weather_station_data_and_locations.groupby(
+            [pd.Grouper(key='date', freq=time_window_frequency), 'station_id']).mean()
+        time_windows = [index[0] for index in grouped_training_data.index]
+
+        for time_window in time_windows:
+            df_window = grouped_training_data.loc[(time_window,)]
+            X = self.X(df_window)
+            y = self.y(df_window)
+
+            # Filter out NaN values
+            non_nan_indexes = np.where(~np.isnan(y))[0]
+            X = X[non_nan_indexes]
+            y = y[non_nan_indexes]
+
+            yield (X, y), time_window
+
+    def transform(self, extract_directory, transform_directory):
+        import config
+
+        # Load required files
+        self.load_extract_files(extract_directory=extract_directory, transform_directory=transform_directory)
 
         # Interpolate for each township the annual temperature, save output "FINAL"
-        df_townships_interpolated = interpolate(ws_yearly_average_temperature=df_ws_yearly_avg_temperature,
-                                                ws_location=weather_station_locations,
-                                                df_townships_centroids=df_townships_centroids)
+        X_interpolate_locations = self.X_interpolate_locations
+        X_interpolate_labels = self.X_interpolate_labels
+        time_window_frequency = self.time_window_frequency
 
+        # Dataframe holding ALL values
+        df = pd.DataFrame(columns=['township', 'date', 'interpolated_values'])
+
+        for (X, y), time_window in self.training_data(time_window_frequency):
+            interpolated_values = interpolate(X=X, X_interpolate_locations=X_interpolate_locations, y=y)
+
+            df_time_window = pd.DataFrame({
+                'township': X_interpolate_labels,
+                'date': time_window,
+                'interpolated_values': interpolated_values
+            })
+
+            df = df.append(df_time_window)
+
+        # Save dataframe
         save_dataframe_to_csv(
-            path=transform_directory / f'township_interpolated_temperatures_{FINAL_TRANSFORMATION_ID}.csv',
-            dataframe=df_townships_interpolated)
+            path=transform_directory / f'township_interpolated_{config.FINAL_TRANSFORMATION_ID}.csv',
+            dataframe=df)
 
 
-class BioClim_2(Base):
-    pass
+# BIO1 = Annual Mean Temperature
+class BioClim_1(BioClim):
+
+    def __init__(self):
+        super().__init__(time_window_frequency='Y')
+
+    def y(self, dataframe):
+        # Filter out irrelevant columns
+        df_avg_temperature = dataframe[['temperature_avg']].values
+
+        return df_avg_temperature
+
+
