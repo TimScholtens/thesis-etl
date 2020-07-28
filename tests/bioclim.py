a3bda785b1f2a3d9a6b7cc61c12b8cd0080975b8
Tim <timscholtens@hotmail.com>
2020-07-28 15:39:48 +0200

    added test for bioclim 17

--- tests/bioclim.py
+++ tests/bioclim.py
@@ -1,10 +1,16 @@
 import unittest
-from etl.transform.transformers.bioclim import BioClim1TimePartitionStrategy, BioClim8TimePartitionStrategy, get_weather_station_values
+from etl.transform.transformers.bioclim import (
+
+    BioClim1TimePartitionStrategy,
+    BioClim8TimePartitionStrategy,
+    BioClim17TimePartitionStrategy,
+    get_weather_station_values
+)
 from pathlib import Path
+import numpy as np
 
 
 class BioClimTransformerTestCases(unittest.TestCase):
-    longMessage = True
 
     @classmethod
     def setUpClass(cls):
@@ -12,9 +18,16 @@ class BioClimTransformerTestCases(unittest.TestCase):
         extract_directory = Path.cwd() / 'static'
         cls.weather_station_values = get_weather_station_values(extract_directory)
 
+        # Set max percentage tolerance
+        cls.MAX_PERCENT_DEVIATION = 5
+
     @classmethod
     def tearDownClass(cls):
         cls.weather_station_values = None
+
+    def log(self, metric_id, expected_value, calculated_value):
+        print(f'\t Expected {metric_id}: {expected_value}, \n '
+              f'\t Calculated {metric_id}: {calculated_value}')
 
     def test_bioclim1_time_partition_strategy(self):
         """
@@ -24,10 +37,14 @@ class BioClimTransformerTestCases(unittest.TestCase):
         expected_average_temp = 10.62868852  # From spreadsheet in google drive
         calculated_average_temp = df_year['temperature_avg'].values[0]
 
-        # Compare if values are correct up to 4 decimals
-        self.assertAlmostEqual(expected_average_temp, calculated_average_temp, 4)
-        print(f'\t Expected average temperature: {expected_average_temp}, \n '
-              f'\t Calculated average temperature: {calculated_average_temp}')
+        # Compare if values are match within 5% tolerance
+        np.testing.assert_allclose(actual=calculated_average_temp,
+                                   desired=expected_average_temp,
+                                   rtol=self.MAX_PERCENT_DEVIATION)
+
+        self.log(metric_id='temperature_avg',
+                 expected_value=expected_average_temp,
+                 calculated_value=calculated_average_temp)
 
     def test_bioclim8_time_partition_strategy(self):
         """
@@ -40,6 +57,24 @@ class BioClimTransformerTestCases(unittest.TestCase):
         # expected_average_temperature = 12.39262295
 
         self.assertEqual(True, True)
+
+    def test_bioclim17_time_partition_strategy(self):
+        """
+            Strategy must return the total precipitation of the driest quarter.
+        """
+        df_year = BioClim17TimePartitionStrategy().aggregate(self.weather_station_values)
+
+        expected_sum_rainfall = 103.3  # in mm
+        calculated_sum_rainfall = df_year['rain_sum'][0]
+
+        # Compare if values are match within 5% tolerance
+        np.testing.assert_allclose(actual=calculated_sum_rainfall,
+                                   desired=expected_sum_rainfall,
+                                   rtol=self.MAX_PERCENT_DEVIATION)
+
+        self.log(metric_id='rain_sum',
+                 expected_value=expected_sum_rainfall,
+                 calculated_value=calculated_sum_rainfall)
 
 
 if __name__ == '__main__':
